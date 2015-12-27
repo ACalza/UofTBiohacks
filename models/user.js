4a02639f9625ca4c1d3c46f9dc55231ab0e1e03b
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-12-26 19:11:01 -0500

    deleted olds

--- models/user.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-//Create and compile schema
-var userSchema = mongoose.Schema({
-    email: String
-});
-
-module.exports = mongoose.model('user', userSchema);;
