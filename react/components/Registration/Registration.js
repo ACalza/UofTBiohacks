4945fbe3f7e6fc1942c3cf8e00c348ef4a710c03
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-12-28 17:31:15 -0500

    loaded koistya/react-static-boilerplate, made custom Registation component

---
+++ react/components/Registration/Registration.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Registration() {
+    return (
+        <div> wheeeee </div>
+    )
+}
+
+export default Registration
---
+++ react/pages/index.js
@@ -0,0 +1,22 @@
+/**
+ * React Static Boilerplate
+ * https://github.com/koistya/react-static-boilerplate
+ * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
+ */
+
+import React, { Component } from 'react';
+import Registration from '../components/Registration'
+
+export default class extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>Coming soon. (now)</p>
+        <Registration />
+      </div>
+    );
+  }
+
+}
