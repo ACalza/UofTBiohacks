5a390e0cf16d8a32584d3737a3ea1730627fd4a9
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-12-28 19:19:49 -0500

    builds inputs from requiring reg-form.js

--- react/components/Registration/Registration.js
+++ react/components/Registration/Registration.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react'
 import Input from '../Input'
 
+// import User from '../../../src/models/user.js'
+import User from '../../../reg-form.js'
+
 class Registration extends Component {
   render() {
     return (
        <div>
-        sdsdsd
-        <Input
-          placeholder="username"
-        />
+        {User.map(item =>
+          <Input
+            placeholder={item.val}
+          />
+        )}
        </div>
     )
   }
---
+++ reg-form.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    val: 'Name',
+    required: true,
+    type: 'text'
+  },
+  {
+    val: 'Email',
+    required: true,
+    type: 'text'
+  }
+]
