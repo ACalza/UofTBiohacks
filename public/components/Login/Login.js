76397a89ebaa1e59db6b795a43dae6fb0400faca
ACalza <albert.calzaretto@gmail.com>
2016-01-04 15:17:48 -0500

    Started on login, still issues with webkit

--- public/components/Login/Login.js
+++ public/components/Login/Login.js
@@ -1,19 +1,66 @@
 import React, {Component} from 'react'
-
+import FMUI, {FormsyText}  from 'formsy-material-ui'
+import RaisedButton from 'material-ui/lib/raised-button'
 import autobind from 'autobind-decorator'
 import $ from 'jquery'
 
 
+@autobind
 export default class Login extends Component {
-  constructor(props) {
-    super(props)
-    // TODO generate state properties from user-registration.js
+    constructor(props) {
+        super(props)
+        this.state = {
+            canSubmit: false
+        }
+        this.errorMessages = {
+            wordsError: "Please fill in the entire form"
+        }
     }
+    enableButton(){
+        this.setState({
+            canSubmit: true
+        })
+    }
+    disableButton(){
+        this.setState({
+            canSubmit: false
+        })
+    }
+    submitForm(model){
+        console.log("model: " + model)
+    }
+
     render() {
-       return (
-         <div className="login">
-           Hello world
-         </div>
-       );
-   }
+        let {wordsError} = this.errorMessages
+        return (
+
+            <Formsy.Form
+                onValid={this.enableButton}
+                onInvalid={this.disableButton}
+                onValidSubmit={this.submitForm}>
+
+                <FormsyText
+                    name='emailOrUsername'
+                    validationError={wordsError}
+                    required
+                    hintText="What is your Email or Username?"
+                    floatingLabelText="Email or Username"
+                />
+                <br></br>
+                <FormsyText
+                    name='password'
+                    validationError={wordsError}
+                    required
+                    hintText="What is your password?"
+                    floatingLabelText="Password"
+                />
+                <br></br>
+                <RaisedButton
+                    type="submit"
+                    label="Submit"
+                disabled={!this.state.canSubmit}
+                 />
+            </Formsy.Form>
+        );
+    }
 }
---
+++ public/components/field-types/TextField.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+export default class TextField extends Component {
+  constructor(props) {
+    super(props)
+   }
+   render() {
+       return (
+            <input type="text"/>
+       );
+   }
+}
