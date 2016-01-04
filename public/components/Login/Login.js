9753a26329820ed7da2645b2d7bafe66cc13b88b
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-01-04 16:00:10 -0500

    fixed react-material-ui server render issue; see pages/login.js

--- public/components/Login/Login.js
+++ public/components/Login/Login.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import FMUI, {FormsyText}  from 'formsy-material-ui'
+import React, { Component } from 'react'
+import FMUI, { FormsyText } from 'formsy-material-ui'
 import RaisedButton from 'material-ui/lib/raised-button'
 import autobind from 'autobind-decorator'
 import $ from 'jquery'
@@ -7,60 +7,57 @@ import $ from 'jquery'
 
 @autobind
 export default class Login extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            canSubmit: false
-        }
-        this.errorMessages = {
-            wordsError: "Please fill in the entire form"
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      canSubmit: false
     }
-    enableButton(){
-        this.setState({
-            canSubmit: true
-        })
+    this.errorMessages = {
+      wordsError: 'Please fill in the entire form'
     }
-    disableButton(){
-        this.setState({
-            canSubmit: false
-        })
-    }
-    submitForm(model){
-        console.log("model: " + model)
-    }
+  }
+  enableButton() {
+    this.setState({
+      canSubmit: true
+    })
+  }
+  disableButton() {
+    this.setState({
+      canSubmit: false
+    })
+  }
+  submitForm(model) {
+    console.log('model: ' + JSON.stringify(model))
+  }
 
-    render() {
-        let {wordsError} = this.errorMessages
-        return (
+  render() {
+    let { wordsError } = this.errorMessages
+    return (
+      <Formsy.Form
+        onValid = {this.enableButton}
+        onInvalid = {this.disableButton}
+        onValidSubmit = {this.submitForm}>
 
-            <Formsy.Form
-                onValid={this.enableButton}
-                onInvalid={this.disableButton}
-                onValidSubmit={this.submitForm}>
+        <FormsyText style={{display: 'block'}}
+          name = 'emailOrUsername'
+          validationError = {wordsError}
+          required hintText = "What is your Email or Username?"
+          floatingLabelText = "Email or Username"
+        />
 
-                <FormsyText
-                    name='emailOrUsername'
-                    validationError={wordsError}
-                    required
-                    hintText="What is your Email or Username?"
-                    floatingLabelText="Email or Username"
-                />
-                <br></br>
-                <FormsyText
-                    name='password'
-                    validationError={wordsError}
-                    required
-                    hintText="What is your password?"
-                    floatingLabelText="Password"
-                />
-                <br></br>
-                <RaisedButton
-                    type="submit"
-                    label="Submit"
-                disabled={!this.state.canSubmit}
-                 />
-            </Formsy.Form>
-        );
-    }
+        <FormsyText style={{display: 'block'}}
+          name = 'password'
+          validationError = {wordsError}
+          required hintText = "What is your password?"
+          floatingLabelText = "Password"
+        />
+
+        <RaisedButton
+          type = "submit"
+          label = "Submit"
+          disabled = {!this.state.canSubmit}
+          />
+      </Formsy.Form>
+    )
+  }
 }
--- public/pages/login.js
+++ public/pages/login.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment'
+
 import Login from '../components/Login/Login.js'
 
 export default class extends Component {
 
   render() {
-    return (
-        <Login />
-    );
+    let content = <div></div>
+    if (canUseDOM) {
+      content = <Login />
+    }
+
+    return (content)
   }
 
 }
