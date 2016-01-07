b53526e0cc846881fc0d7f3cd05e8c25a720e0ea
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-01-07 17:02:04 -0500

    broken formsy ffs

--- app/src/app.js
+++ app/src/app.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { Router, Route, Link } from 'react-router'
 import { Provider } from 'react-redux'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import configureStore from './store/configureStore'
 
@@ -11,6 +12,9 @@ import configureStore from './store/configureStore'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+
+// Required for material-ui
+injectTapEventPlugin()
 
 // App container setup
 let appContainer = document.createElement('div')
@@ -21,14 +25,36 @@ document.body.appendChild(appContainer)
 const store = configureStore()
 
 
+import FMUI, { FormsyText, FormsyToggle } from 'formsy-material-ui'
+import RaisedButton from 'material-ui/lib/raised-button'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={createBrowserHistory()}>
-      <Route path="/" component={Home} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
-    </Router>
-  </Provider>,
+  <Formsy.Form>
+      onValid = {() => alert('valid')}
+      onInvalid = {() => alert('invalid')}
+      onValidSubmit = {() => alert('validsubmit')}>
+
+      <FormsyText style={{display: 'block'}}
+        required
+        name = 'email'
+        validations="isEmail"
+        validationError={'Invalid email'}
+        hintText = "What is your email?"
+        floatingLabelText = "Email"
+      />
+
+    <RaisedButton> Register </RaisedButton>
+  </Formsy.Form>,
   appContainer
 )
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={createBrowserHistory()}>
+//       <Route path="/" component={Home} />
+//       <Route path="/register" component={Register} />
+//       <Route path="/login" component={Login} />
+//     </Router>
+//   </Provider>,
+//   appContainer
+// )
--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
 
+import FMUI, { FormsyText, FormsyToggle } from 'formsy-material-ui'
+import RaisedButton from 'material-ui/lib/raised-button'
+
+
 export default class Register extends Component {
   render() {
     return (
       <Layout title="Register">
-        Home content as children.
+        <Formsy.Form>
+          <FormsyToggle
+          name='toggle'
+          label="Toggle" />
+          
+          <RaisedButton> Register </RaisedButton>
+        </Formsy.Form>
       </Layout>
     )
   }
 }
+
+// export default class Register extends Component {
+//   render() {
+//     return (
+//       <Layout title="Register">
+//         <Formsy.Form>
+//           <FormsyText
+//             required
+//             name='name'
+//             validations='isWords'
+//             hintText="What is your name?"
+//             value="Bob"
+//             floatingLabelText="Name"
+//           />
+//
+//           <RaisedButton> Register </RaisedButton>
+//       </Formsy.Form>
+//       </Layout>
+//     )
+//   }
+// }
