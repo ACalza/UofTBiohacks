e470de3c5af28139e8a73ec6f65746c178b18f23
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-01-07 12:44:29 -0500

    css to capitalize navbar links

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -2,21 +2,23 @@ import React from 'react'
 import PureComponent from 'react-pure-render/component'
 import { Link } from 'react-router'
 
+import '../styles/Navigation.scss'
+
 export default class Button extends PureComponent {
   render() {
     const { links } = this.props
 
     return(
-      <nav>
+      <nav className="Navigation">
         {links.map( (link, i) => {
           let linkName
           if (link === '/') {
-            linkName = 'Home'
+            linkName = 'home'
           } else {
             linkName = link.substr(1)
           }
 
-          return <Link to={link} key={i}> {linkName} </Link>
+          return <Link to={link} key={i} className="navLink"> {linkName} </Link>
         })}
       </nav>
     )
