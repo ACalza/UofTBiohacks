9405d24f081a93b2ac8dcdee2647da41f82a6b8c
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-01-10 04:52:18 -0500

    responsive logo homepage

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -18,18 +18,18 @@ export default class Home extends Component {
       <Layout>
         <div className="Splash jumbotron">
           <div className="headerPush" />
-          <div>
-            <img src="http://45.55.193.224:1234/BioHacksLogo.svg" width="50%" style={{display: 'block'}}/>
-          </div>
+          <img src="http://45.55.193.224:1234/BioHacksLogo.svg" />
           <h1>UofT BioHacks</h1>
         </div>
-        <div className="container">
-          This is the super awesome homepage.
-          <Row className="show-grid">
-            <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-            <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-            <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
-          </Row>
+        <div style={{paddingTop: '30px', backgroundColor: '#eee'}}>
+          <div className="container">
+            This is the super awesome homepage.
+            <Row className="show-grid">
+              <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+              <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+              <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+            </Row>
+          </div>
         </div>
       </Layout>
     )
