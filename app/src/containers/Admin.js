f7629368305734fc5e001fc091f4ae3b94c58dd3
ACalza <albert.calzaretto@gmail.com>
2016-01-10 18:15:07 -0500

    Downloading CSV is an issue for some reason, not wokring

--- app/src/containers/Admin.js
+++ app/src/containers/Admin.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { ajaxGet } from '../util'
 import {Table} from 'reactable'
+import RaisedButton from 'material-ui/lib/raised-button'
+import $ from 'jquery'
+
+import { BASE_URI } from '../constants/uris.js'
+
 
 export default class Admin extends Component {
   constructor(){
     super()
     this.state = {
-      tableLoaded: false,
       users: []
     }
   }
@@ -17,16 +21,38 @@ export default class Admin extends Component {
       if (err) {
         console.error(err)
       } else {
-        console.log(data)
-
         this.setState({
-          tableLoaded: true,
           users: data
         })
       }
     })
   };
+  exportAsCSV = () => {
+    let jwt = this.props.jwt
+    let url = BASE_URI + '/user/all/csv'
+    $.ajax({
+    type: "POST",
+    url: url,
+    data: {},
+    beforeSend: function (request)
+    {
+       request.setRequestHeader('Authorization', 'Bearer ' + jwt);
+    },
+    success: function(response, status, request) {
+        var disp = request.getResponseHeader('Content-Disposition');
+        if (disp && disp.search('attachment') != -1) {
+            var form = $('<form method="POST" action="' + url + '">');
+            $.each(params, function(k, v) {
+                form.append($('<input type="hidden" name="' + k +
+                        '" value="' + v + '">'));
+            });
+            $('body').append(form);
+            form.submit();
+        }
+    }
+});
 
+  };
   render() {
     if(!this.props.jwt){
       return <p> Loading </p>
@@ -34,7 +60,17 @@ export default class Admin extends Component {
     return(
       <div className="adminPage">
         <h2>Hello iGem Admin</h2>
-        <Table className="Users" data={this.state.users} />
+        <RaisedButton
+          type = "Submit"
+          label = "Export Users as CSV"
+          onTouchTap = {this.exportAsCSV}
+        />
+        <hr></hr>
+        <h2>List of all users</h2>
+        <p>Features: Clicking on the column names sort the table</p>
+        <Table className="Users"
+          sortable={true}
+          data={this.state.users} />
       </div>
     )
   }
