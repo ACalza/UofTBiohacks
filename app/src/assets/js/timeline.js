a6d7eb1a02ad52616ab12935dc868dfb1cc8d16d
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-01-13 20:34:16 -0500

    timeline generated from JS

---
+++ app/src/assets/js/timeline.js
@@ -0,0 +1,38 @@
+export default {
+  '1:00 p.m.': {
+    'title': 'Arrival and Registration',
+    // 'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.'
+  },
+  '2:00 p.m.': {
+    'title': 'Opening Ceremony',
+    // 'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.'
+  },
+  '3:00 p.m.': {
+    'title': 'Opening Keynote'
+  },
+  '4:00 p.m.': {
+    'title': 'Introduction of Biohackathon and Brainstorming Session'
+  },
+  '4:15 p.m.': {
+    'title': 'Workshops',
+    'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.'
+  },
+  '5:00 p.m.': {
+    'title': 'Biocoding starts!'
+  },
+  '9:00 a.m.': {
+    'title': 'Presentation Sign-Up'
+  },
+  '9:30 a.m.': {
+    'title': 'Presentations',
+  },
+  '10:45 a.m.': {
+    'title': 'Closing Keynote'
+  },
+  '11:30 a.m.': {
+    'title': 'Closing Ceremony'
+  },
+  '12:00 p.m.': {
+    'title': 'Farewell!'
+  }
+}
