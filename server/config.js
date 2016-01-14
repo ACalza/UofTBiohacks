104a0f70e332fa24d655d9dd5795771d5a0664be
ACalza <albert.calzaretto@gmail.com>
2016-01-14 12:49:29 -0500

    Sending email to reset password works from backend

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     SECRET: "adfjostq4tu2489r3892h23h89ipunchedkeyboad",
-    sendgridusername: "iGemUofT",
-    sendgridpassword: "5Vm-WVX-4My-mr2"
+    api_key: "5Vm-WVX-4My-mr2",
+    api_user: "iGemUofT"
 }
