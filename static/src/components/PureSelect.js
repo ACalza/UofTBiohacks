1f403e856e1a9a1b690647f45c93530f816a41eb
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-01-31 14:44:20 -0500

    moving to PureComps still in progress

---
+++ static/src/components/PureSelect.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PureComponent from 'react-pure-render/component'
+
+import { FormsySelect } from 'formsy-material-ui'
+import { MenuItem } from 'material-ui/lib'
+
+export default class PureTextInput extends PureComponent {
+  render() {
+    const {
+      required,
+      name,
+      floatingLabelText,
+      items
+    } = this.props
+
+
+
+    return (
+    <FormsySelect required={required} name={name} floatingLabelText={floatingLabelText}>
+      {items.map( (item, i) =>
+        <MenuItem key={i} value={item.value} primaryText={item.text} />
+      )}
+    </FormsySelect>)
+  }
+}
