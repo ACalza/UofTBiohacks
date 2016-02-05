8b9e2a54de3329542960f90ad6b4b8ad3f1a3141
ACalza <albert.calzaretto@gmail.com>
2016-02-05 11:42:28 -0500

    Fixed #90

--- static/src/register/index.js
+++ static/src/register/index.js
@@ -105,6 +105,7 @@ class Register extends Component {
                 onValidSubmit = {this.submitForm}
                 onValid = {() => dispatch(canSubmit())}
                 onInvalid = {() => dispatch(canNotSubmit())}
+                autocomplete={false}
               >
                 <div className="fullWidth">
                   <PureTextInput
