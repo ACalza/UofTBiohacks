49c79339a3ee8706bff40af059859a1b99ee33c3
ACalza <albert.calzaretto@gmail.com>
2016-03-06 16:31:14 -0500

    Updated /all

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -21,7 +21,7 @@ router.post('/reject', function*(){
   }
 })
 router.get('/all', function*(){
-  let data = yield User.find({isinvited: false})
+  let data = yield User.find({isinvited: undefined, doesAcceptInvite: undefined})
   this.body = {
     data: data
   }
