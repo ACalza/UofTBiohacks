b730c86da28d947d7c5d3578e1123bbac019ee8a
harrig12 <cait.harrigan@mail.utoronto.ca>
2018-01-24 16:53:45 -0500

    2018 timeline updates
    Still missing dates, and speaker/workshop information

--- static/src/assets/js/timeline.js
+++ static/src/assets/js/timeline.js
@@ -1,45 +1,48 @@
 export default {
-  '1:00 p.m.': {
-    'title': 'Arrival and Registration',
+  //March 17th
+  '8:30 a.m. - Atrium': {
+    'title': 'Registration and Team Formation',
     'icon': 'fa fa-sign-in'
   },
-  '2:00 p.m.': {
-    'title': 'Opening Ceremony',
-    'icon': 'fa fa-flash'
+  '9:00 a.m. - Atrium': {
+    'title': 'Breakfast',
+    'icon': 'fa fa-coffee'
   },
-  '2:30 p.m.': {
-    'title': 'Opening Keynote Speech from Hui Yuan Xiong',
-    'icon': 'fa fa-certificate',
-    'content':
-    `
-      <p>
-        Hui Yuan Xiong is the co-founder of Deep Genomics along with Prof. Brendan John Frey, 
-        an Engineering Science graduate and a researcher at Frey Lab. His research in genomics, machine learning
-        and various other areas have led to top-tier academic publications and commercial applications featured in Wired,
-        Scientific American, Quanta, CBC, and the National Post.
-      </p>
-      <div class="headshot-wrapper">
-        <img class="headshot" src="/img/Hui-Xiong.jpg" />
-      </div>
-    `
+  '9:30 a.m. - BA1180': {
+    'title': 'Opening Ceremonies',
+    'icon': 'fa fa-fire'
   },
-  '3:00 p.m.': {
-    'title': 'Workshops',
-    'icon': 'fa fa-sticky-note-o'
+  '10:00 a.m. - BA1180': {
+    'title': 'Opening Keynote Speech',
+    'icon': 'fa fa-key'
   },
-  '4:00 p.m.': {
-    'title': 'Sponsor Demos',
-    'icon': 'fa fa-info',
-    'content': '<p>Stay tuned!</p>'
-  },
-  '5:00 p.m.': {
-    'title': `Hacking Starts!`,
+  '11:00 a.m.': {
+    'title': 'Hacking Begins!',
     'icon': 'fa fa-code',
     'content':
     `
-      Work away at a new and interesting problem. Mentors will be available
-      all night.
+      <p>
+        Get comfy in any of the following rooms: BA2179, BA2139, BA2145, 
+        BA3200, BA2270. Mentors will be circulating to answer questions!
+      </p>
     `
+  },
+  '12:00 p.m. - Atrium': {
+    'title': 'Lunch',
+    'icon': 'fa fa-cutlery'
+  },
+  '4:00 p.m. - BA1180': {
+    'title': 'Workshops',
+    'icon': 'fa fa-cog'
+  },
+  '7:00 p.m. - Atrium': {
+    'title': 'Dinner',
+    'icon': 'fa fa-cutlery'
+  },
+  //March 18th
+  '9:00 a.m. - Atrium': {
+    'title': 'Breakfast',
+    'icon': 'fa fa-coffee'
   },
   '9:00 a.m.': {
     'title': 'Presentation Sign-Up',
@@ -50,24 +53,11 @@ export default {
     'icon': 'fa fa-laptop'
   },
   '11:30 a.m.': {
-    // Please insert AlanDavidson.jpg here in a bubble on the side
-    'title': 'Closing Keynote Speech from Prof. Alan Davidson',
-    'icon': 'fa fa-language',
-    'content':
-    `
-      <p>
-        Alan Davidson is a Professor at University of Toronto Department of
-        Molecular Genetics and Department of Biochemistry. He is well-known with his
-        research in bioinformatics, protein structure determination, in vitro
-        biochemical studies and in vivo functional studies.
-      </p>
-      <div class="headshot-wrapper">
-        <img class="headshot" src="/img/AlanDavidson.jpg" />
-      </div>
-    `
+    'title': 'Closing Keynote Speech',
+    'icon': 'fa fa-key'
   },
   '12:00 p.m.': {
     'title': 'Closing Ceremony',
-    'icon': 'fa fa-fire'
+    'icon': 'fa fa-trophy'
   }
 }
