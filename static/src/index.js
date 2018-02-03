63b5960dc3eea4b5aa18f7e1480413688176fec4
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2018-02-03 15:08:33 -0500

    remove parent div for buttons

--- static/src/index.js
+++ static/src/index.js
@@ -52,8 +52,8 @@ class Index extends Component {
                     <h2 style={{textAlign: 'center'}}><i className="fa fa-calendar"></i> March 17-18</h2>
 
                     <div className="buttonHolder">
-                      <div type="button" className="btn btn-primary btn-lg"><a href="https://goo.gl/forms/V3ujQb3Wt1qNXMYr1">Volunteer</a></div>
-                      <div style={{marginLeft: '20px'}} type="button" className="btn btn-primary btn-lg"><a href="/BCB-biohacks-2018-sponsor-package.pdf">Sponsor</a></div>
+                      <a className="btn btn-primary btn-lg" href="https://goo.gl/forms/V3ujQb3Wt1qNXMYr1">Volunteer</a>
+                      <a style={{marginLeft: '20px'}} className="btn btn-primary btn-lg" href="/BCB-biohacks-2018-sponsor-package.pdf">Sponsor</a>
                     </div>
                   </div>
                   <hr />
