1d808c46972afd40720bda51e704f2045623aa91
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2018-02-09 18:08:51 -0500

    [app] hardcode baseUri for frontend
    Co-authored-by: Albert Calzaretto <albert.calzaretto@gmail.com>

--- static/src/constants/uris.js
+++ static/src/constants/uris.js
@@ -1,8 +1,12 @@
 let baseUri
 if (__DEV__) {
-  baseUri = 'http://localhost:3000'
+  console.log('USING DEV URL')
+  baseUri = 'http://167.114.148.229:3000'
 } else {
-  baseUri = 'https://api.uoftbiohacks.com'
+  console.log('USERING NON_DEV URL')
+  // TODO not this shit
+  baseUri = 'http://167.114.148.229:3000'
+  // baseUri = 'https://api.uoftbiohacks.com'
 }
 
 export const BASE_URI = baseUri
