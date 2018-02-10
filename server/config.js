951827dda561ec74513ca6ba702e58f28212eb99
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2018-02-09 21:33:02 -0500

    remove shared from config

--- server/config.js
+++ server/config.js
@@ -1,16 +1,6 @@
-const shared = require('../shared/constants.js')
-
-var url = null
-if(process.env.NODE_ENV === 'prod'){
-  url = shared.prod.STATIC_URL
-}else{
-  url = shared.dev.STATIC_URL
-}
+'use strict'
 
 module.exports = {
     SECRET: "adfjostq4tu2489r3892h23h89ipunchedkeyboad",
     admin_pass: "W7Gs67ep6s57DDpfqC4EQt",
-    FRONT_END_URL: url
 }
-
-// get 'prod' config from shared using process.env.NODE_ENV === 'prod' or 'dev'
